96634648822e6dcaf9b9fe3d9d07806d0ca1c8f9
Riya <riyabanaulikar@gmail.com>
2025-01-13 23:09:12 +0530

    user register and user login with authentication

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectDB();
 app.use(express.json());
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 
 const PORT = process.env.PORT || 5000;
