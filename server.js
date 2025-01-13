4e2b1b32002e405a063e7219e84bb8b3ac16b5f7
Riya <riyabanaulikar@gmail.com>
2025-01-13 21:04:46 +0530

    first demo route created

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json());
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
